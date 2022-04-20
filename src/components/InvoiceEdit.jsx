5bfe2940561a7d623a29ff006dc1eb1231b8ba4d
unknown <andrei_g_git@protonmail.com>
2022-04-20 16:30:24 -0700

    add submit edit to server functionality

--- src/components/InvoiceEdit.jsx
+++ src/components/InvoiceEdit.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { editorToggled } from '../redux/actions';
+import { 
+    editorToggled,
+    nameChanged,
+    amountChanged,
+    statusChanged,
+    countryChanged,
+    cityChanged,
+    phoneChanged 
+} from '../redux/actions';
 import "../css/InvoiceEdit.scss";
 
 const $ = require("jquery");
@@ -10,42 +18,48 @@ export const InvoiceEdit = (props) => {
     <div className="edit-form-container">     
         <form className="edit-form"
             data-testid="edit-form"
-            onSubmit={null}
+            onSubmit={/* () =>  */curryHandleSubmit(props)}
         >
             <label htmlFor="name-field">Customer name</label>
             <input className="" id="name-field"
                 type="text"
+                onChange={(event) => props.handleNameChange(event)}
             />
 
             <label htmlFor="amount-field">Transaction amount</label>
             <input className="" id="amount-field"
                 type="text"
+                onChange={(event) => props.handleAmountChange(event)}
             />
 
             <label htmlFor="status-field">Transaction status</label>
             <input className="" id="status-field"
                 type="text"
+                onChange={(event) => props.handleStatusChange(event)}
             />
 
             <label htmlFor="country-field">Country of origin</label>
             <input className="" id="country-field"
                 type="text"
+                onChange={(event) => props.handleCountryChange(event)}
             />
 
             <label htmlFor="city-field">City of residence</label>
             <input className="" id="city-field"
                 type="text"
+                onChange={(event) => props.handleCityChange(event)}
             />
 
             <label htmlFor="phone-field">Phone number</label>
             <input className="" id="phone-field"
                 type="text"
+                onChange={(event) => props.handlePhoneChange(event)}
             />
 
             <input className=""
-                type="submit"
+                type="submit"  //--- apparently "submit" disconnects the form if btn has click event
                 value="File Invoice"
-                onClick={() => props.toggleEditor(! props.editorOpen)}
+                //onClick={() => props.toggleEditor(! props.editorOpen)}
             />
 
             
@@ -54,34 +68,53 @@ export const InvoiceEdit = (props) => {
   )
 }
 
-const handleSubmit = () => {
-    
+const curryHandleSubmit = (props) => {
+    console.log("curry handle submit" + props.changes)
+    return function(event) {
+        console.log("curry handle submit")
+
+        const changes = props.changes;
+
+        event.preventDefault();
+
+        $.ajax({
+            type: "POST",
+            url: "/api/invoices/add",
+            data: { 
+                //invoice: {
+                    name: changes.name,
+                    order: 999,//changes.order,
+                    date: "99-99-99",//changes.date,
+                    amount: changes.amount,
+                    status: changes.status,
+                    country: changes.country,
+                    city: changes.city,
+                    phone: changes.phone
+            /* } */},
+            success: response => {
+                console.log(response)
+                //props.fetchSearchedGames(response)
+            }        
+        });
+
+        props.toggleEditor(! props.editorOpen);
+    }
 }
 
 const uploadInvoice = (data) => {
-    $.ajax({
-        type: "POST",
-        url: "/api/invoices",
-        data: { invoice: {
-            name: data.name,
-            order: data.order,
-            date: data.date,
-            amount: data.amount,
-            status: data.status,
-            country: data.country,
-            city: data.city,
-            phone: data.phone
-        }},
-        success: response => {
-            console.log(response)
-            //props.fetchSearchedGames(response)
-        }        
-    })
+    
 };
 
 const mapStateToProps = (state) => {
 	return {
 		editorOpen: state.ui.editorOpen,
+        // name: state.ui.edit.name,
+        // amount: state.ui.edit.amount,
+        // status: state.ui.edit.state,
+        // country: state.ui.edit.country,
+        // city: state.ui.edit.city,
+        // phone: state.ui.edit.phone
+        changes: state.ui.edit
 	}
 };
 
@@ -89,7 +122,37 @@ const mapDispatchToProps = (dispatch) => {
 	return {
 		toggleEditor: (isOpen) => {
 			dispatch(editorToggled(isOpen));
-		}
+		},
+        handleNameChange: (event) => {
+            //return (event) => {
+                dispatch(nameChanged(event.target.value));
+            //}
+        },
+        handleAmountChange: (event) => {
+            //return (event) => {
+                dispatch(amountChanged(event.target.value));
+            //}
+        },
+        handleStatusChange: (event) => {
+            //return (event) => {
+                dispatch(statusChanged(event.target.value));
+            //}
+        },
+        handleCountryChange: (event) => {
+            //return (event) => {
+                dispatch(countryChanged(event.target.value));
+            //}
+        },
+        handleCityChange: (event) => {
+            //return (event) => {
+                dispatch(cityChanged(event.target.value));
+            //}
+        },
+        handlePhoneChange: (event) => {
+            //return (event) => {
+                dispatch(phoneChanged(event.target.value));
+            //}
+        }                                        
 	}
 }
 
