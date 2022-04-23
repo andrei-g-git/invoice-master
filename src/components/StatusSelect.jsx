77bd7f2a8a359924dc3bb131f71f7c1ff035bf30
unknown <andrei_g_git@protonmail.com>
2022-04-22 18:11:11 -0700

    fix editor dropdown for existing invoices

--- src/components/StatusSelect.jsx
+++ src/components/StatusSelect.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 function StatusSelect(props) {
+
+    const [content, setContent] = useState(props.content);
+
+    useEffect(() => {
+        props.notifyChange({
+            target: {
+                value: content
+            }
+        }); //not a change, but otherwise only input changes will update the store with the form values to be submitted
+    }, []);
+
     const statuses = new Map();
     statuses.set("OVD", "Overdue");
     statuses.set("PEN", "Pending");
     statuses.set("SOD", "Completed");
 
     let editingExisting = false;
-    let selected = false;
+    //let selected = false;
     if(props.content !== undefined && props.content !== ""){
         editingExisting = true;
-        selected = true;
+        //selected = true; //this is silly
     }
+
+    const selectedList = determineIfSelected(statuses, props.options, props.content);
 
     return (
         <select className="status-select"
             name="testing name"
             required
-            onChange={(event) => props.notifyChange(event)}
+            onChange={(event) => { 
+                props.notifyChange(event); //from parent component
+                setContent(event.target.value)
+            }} 
         >
             {
                 editingExisting ? 
@@ -37,7 +53,7 @@ function StatusSelect(props) {
                     <option className="status-value"
                         value={item.value}
                         key={item.id}
-                        selected={selected} //apparently react is smart enough to omit the attribute if the value is false
+                        selected={selectedList[item.id]}//{selected} //apparently react is smart enough to omit the attribute if the value is false
                     >
                         {item.value}
                     </option>
@@ -45,6 +61,22 @@ function StatusSelect(props) {
             }
         </select>
   )
+}
+
+const determineIfSelected = (statuses, options, value) => {
+    let selectedList = [
+        false, 
+        false, 
+        false
+    ];    
+    if(value !== undefined && value !== ""){
+
+        const selectedOptionObject = options.filter(item => item.value == statuses.get(value));
+        const index = selectedOptionObject[0].id;   
+        selectedList[index] = true;     
+    }
+
+    return selectedList;
 }
 
 export default StatusSelect;
